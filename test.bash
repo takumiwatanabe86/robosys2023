4efd4bdea6ec26f7668847a488b80e2b94d4d29f
Takumi Watanabe <takumi@tkm-desk2.localdomain>
2023-10-26 13:02:50 +0900

    kakuninn

--- test.bash
+++ test.bash
@@ -1,3 +1,18 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Takumi Watanabe
+# SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng () {
+	echo NG at Line $1
+	ret=1
+}
+
+ret=0
+
+### I/O TEST ###
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$ret" = 0 ] && echo OK
+exit $ret
